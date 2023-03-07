71e2367b1efd4addb158ca03a725bd1731d94c63
MaylaSilva <may.siqueira93@gmail.com>
2023-03-06 22:25:02 -0300

    Primeiro commit

---
+++ src/test/java/ada/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
